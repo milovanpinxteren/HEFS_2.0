f156e02351fdde73b4bf0658f0f46697ee5852ad
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-03-04 15:57:39 +0100

    add Procfile

---
+++ worker.py
@@ -0,0 +1,26 @@
+# import os
+# import urllib.parse
+#
+# import django
+# import redis
+# from redis import Redis
+# from rq import Worker, Queue, Connection
+#
+# django.setup()
+# listen = ['high', 'default', 'low']
+#
+# redis_url = os.getenv('REDIS_URL', 'localhost:6379')
+#
+# urllib.parse.uses_netloc.append('redis')
+# url = urllib.parse.urlparse(redis_url)
+# conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+#
+#
+# if __name__ == '__main__':
+#     with Connection(conn):
+#         worker = Worker(map(Queue, listen))
+#         worker.work()
+#
+#
+#
+#
