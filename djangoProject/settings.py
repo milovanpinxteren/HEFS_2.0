a59f52a4029fadaf27fee18c22f16f6b36fa9b73
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-03-04 16:20:48 +0100

    Set database_url from env

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hefs2',
+        'NAME': 'hefs',
 
         'USER': 'postgres',
 
@@ -88,6 +88,10 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
