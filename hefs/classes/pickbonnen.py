f750e0c3bb90621dd3d6b4fef684eb9ff8880312
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-03-26 20:34:04 +0200

    feat: picks on good location on pdf, also extra page on more picks

--- hefs/classes/pickbonnen.py
+++ hefs/classes/pickbonnen.py
@@ -49,9 +49,35 @@ class Pickbonnen(fpdf.FPDF):
         self.set_font('helvetica', '', 10)
         self.klantcell(naw)
 
-    def pick_function(self, pick, pickcount):
+    def next_page_cell(self, conversieID, page_nr):
+        self.set_font('helvetica', 'B', 25)
+        self.write(0, '                                ' + str(conversieID) + ' Pagina ' + str(page_nr))
+
+    def pick_function(self, pick, pickcount, conversieID):
         self.set_font('helvetica', '', 10)
         self.set_y(55 + (pickcount * 10))
-        self.pickcell(pick, 8)
+        self.set_x(8)
+        offset_x = 8
+        if 20 < pickcount < 35:
+            offset_x = 100
+            self.set_y(55 + ((pickcount - 21) * 10))
+        if pickcount == 35:
+            self.next_page_cell(conversieID, 2)
+        if 35 < pickcount < 60:
+            print('asdfasdf')
+            print(pick)
+            # self.add_page()
+            offset_x = 8
+            self.set_y(10 + ((pickcount - 35) * 10))
+            # self.set_y(55)
+        if 60 <= pickcount < 85:
+            offset_x = 100
+            self.set_y(15 + ((pickcount - 60) * 10))
+        if pickcount == 85:
+            self.next_page_cell(conversieID, 3)
+        if 85 < pickcount < 100:
+            offset_x = 8
+            self.set_y(15 + ((pickcount - 80) * 10))
+        self.pickcell(pick, offset_x)
 
 
