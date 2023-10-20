8db4d8bf586c42e43fc13e9200327823bce24ff5
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-10-20 13:32:17 +0200

    feat: facturen page

--- hefs/classes/make_factuur_overview.py
+++ hefs/classes/make_factuur_overview.py
@@ -16,8 +16,8 @@ class MakeFactuurOverview():
             calculated_values = [
                 [order_price],
                 [order_price - shipping_cost],
-                [order_price / 1.21],
-                [(order_price / 1.21) * 0.85]
+                [(order_price - shipping_cost) / 1.21],
+                [((order_price - shipping_cost) / 1.21) * 0.85]
             ]
 
             result_dict[conversie_id] = calculated_values
