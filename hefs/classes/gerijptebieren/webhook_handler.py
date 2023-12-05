1785030e4ca7db9a2d0cfd47b8623a054caf142b
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-12-05 18:32:39 +0100

    push migration of error log table

--- hefs/classes/gerijptebieren/webhook_handler.py
+++ hefs/classes/gerijptebieren/webhook_handler.py
@@ -33,7 +33,8 @@ class WebhookHandler():
 
         if request_domain == 'gerijptebieren.myshopify.com':
             if headers["x-shopify-topic"] == "products/delete":
-                self.delete_product(json_body)
+                print('delete product')
+                # self.delete_product(json_body)
             elif headers["x-shopify-topic"] == "products/create":
                 product_creator = ProductCreator()
                 product_creator.create_product(json_body)
@@ -53,24 +54,24 @@ class WebhookHandler():
 
 
 #TODO: does not work, ID does not exist after deletion -> no way to get the handle
-    def delete_product(self, json_body):
-        get_product_original_site_url = f"https://gerijptebieren.myshopify.com/admin/api/2023-10/products/{json_body['id']}.json"
-        original_headers = {"Accept": "application/json", "Content-Type": "application/json",
-                            "X-Shopify-Access-Token": settings.GERIJPTEBIEREN_ACCESS_TOKEN}
-        get_product_original_site_response = requests.get(url=get_product_original_site_url, headers=original_headers)
-        product_handle = get_product_original_site_response.json()['product']['handle']
-
-        partner_websites = {'387f61-2.myshopify.com': settings.GEREIFTEBIERE_ACCESS_TOKEN}  # add domains here
-        for domain_name, token in partner_websites.items():
-            headers = {"Accept": "application/json", "Content-Type": "application/json",
-                       "X-Shopify-Access-Token": token}
-            get_product_on_partner_site_url = f"https://{domain_name}/products/{product_handle}.json"
-            product_on_partner_response = requests.get(url=get_product_on_partner_site_url, headers=headers)
-            if product_on_partner_response.status_code == 200:  # product handle found
-                product_id = product_on_partner_response.json()['product']['id']
-                del_product_on_partner_site_url = f"https://{domain_name}/admin/api/2023-10/products/{product_id}.json"
-                del_product_on_partner_response = requests.delete(url=del_product_on_partner_site_url, headers=headers)
-                print('deleted product on partner site', del_product_on_partner_response)
+    # def delete_product(self, json_body):
+    #     get_product_original_site_url = f"https://gerijptebieren.myshopify.com/admin/api/2023-10/products/{json_body['id']}.json"
+    #     original_headers = {"Accept": "application/json", "Content-Type": "application/json",
+    #                         "X-Shopify-Access-Token": settings.GERIJPTEBIEREN_ACCESS_TOKEN}
+    #     get_product_original_site_response = requests.get(url=get_product_original_site_url, headers=original_headers)
+    #     product_handle = get_product_original_site_response.json()['product']['handle']
+    #
+    #     partner_websites = {'387f61-2.myshopify.com': settings.GEREIFTEBIERE_ACCESS_TOKEN}  # add domains here
+    #     for domain_name, token in partner_websites.items():
+    #         headers = {"Accept": "application/json", "Content-Type": "application/json",
+    #                    "X-Shopify-Access-Token": token}
+    #         get_product_on_partner_site_url = f"https://{domain_name}/products/{product_handle}.json"
+    #         product_on_partner_response = requests.get(url=get_product_on_partner_site_url, headers=headers)
+    #         if product_on_partner_response.status_code == 200:  # product handle found
+    #             product_id = product_on_partner_response.json()['product']['id']
+    #             del_product_on_partner_site_url = f"https://{domain_name}/admin/api/2023-10/products/{product_id}.json"
+    #             del_product_on_partner_response = requests.delete(url=del_product_on_partner_site_url, headers=headers)
+    #             print('deleted product on partner site', del_product_on_partner_response)
 
 
 
