e70fccf5f30350c049cf71aa736899e597be234e
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-02-29 18:03:00 +0100

    feat: code cleanup

--- djangoProject/tasks.py
+++ djangoProject/tasks.py
@@ -1,10 +1,6 @@
 from hefs.classes.microcash_sync.ftp_getter import FTPGetter
-
-import time
 from django_rq import get_queue
-from rq.registry import ScheduledJobRegistry
 from django.conf import settings
-from djangoProject.tasks import update_product_inventory, sync_all_products
 from datetime import datetime, timedelta
 
 
@@ -17,3 +13,9 @@ def update_product_inventory():
 def sync_all_products():
     ftp_getter = FTPGetter()
     ftp_getter.get_ftp_full_file()
+    queue = get_queue()
+    now = datetime.now()
+    next_run = datetime(now.year, now.month, now.day, 22, 30)
+    if now > next_run:
+        next_run += timedelta(days=1)
+    queue.enqueue_at(next_run, sync_all_products)
