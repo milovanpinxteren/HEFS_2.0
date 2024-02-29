bc284bbb1bf78fdd267b9adf9c0aeb31a35aca5c
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-02-29 15:01:51 +0100

    feat: add python version to runtime.txt

--- hefs/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-
-from hefs.classes.microcash_sync.ftp_getter import FTPGetter
-
-
-@shared_task
-def sync_changed_inventory():
-    ftp_getter = FTPGetter()
-    ftp_getter.get_ftp_changed_file()
