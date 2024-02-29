1d0da9762e9803d4e9ad5910468e814bc8f671a4
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-02-29 18:03:00 +0100

    feat: code cleanup

--- djangoProject/scheduler.py
+++ djangoProject/scheduler.py
@@ -1,29 +1,16 @@
-
-import time
-from datetime import timedelta
-from os import getenv
-
-from django.dispatch import receiver
-from django_rq import get_queue
-from rq import Queue
-from rq.registry import ScheduledJobRegistry
-from django.conf import settings
-from redis import Redis
-from djangoProject.tasks import update_product_inventory, sync_all_products
 from datetime import datetime, timedelta
+from django_rq import get_queue
+from djangoProject.tasks import update_product_inventory, sync_all_products
 
 
 def start_schedule_tasks():
-    #init for 5 minute task
+    # init for 5 minute task
     update_product_inventory()
 
-    #init for daily task at 22:30
+    # init for daily task at 22:30
     queue = get_queue()
     now = datetime.now()
     next_run = datetime(now.year, now.month, now.day, 22, 30)
     if now > next_run:
         next_run += timedelta(days=1)
     queue.enqueue_at(next_run, sync_all_products)
-
-
-
