94bf4d4060d76a1a1857616a551d0b8499f7202b
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-02-29 19:07:19 +0100

    feat: increase time of full sync

--- djangoProject/tasks.py
+++ djangoProject/tasks.py
@@ -6,18 +6,17 @@ from datetime import datetime, timedelta
 
 def update_product_inventory():
     ftp_getter = FTPGetter()
-    get_changed_file = ftp_getter.get_ftp_changed_file()
-    get_changed_file()
+    ftp_getter.get_ftp_changed_file()
     queue = get_queue()
     print('enqueing')
-    queue.enqueue_in(timedelta(seconds=int(settings.SCHEDULE_INTERVAL)), get_changed_file)
+    queue.enqueue_in(timedelta(seconds=int(settings.SCHEDULE_INTERVAL)), update_product_inventory)
 
 def sync_all_products():
     ftp_getter = FTPGetter()
     ftp_getter.get_ftp_full_file()
     queue = get_queue()
     now = datetime.now()
-    next_run = datetime(now.year, now.month, now.day, 22, 30)
+    next_run = datetime(now.year, now.month, now.day, 23, 30)
     if now > next_run:
         next_run += timedelta(days=1)
     print('enqueing full sync')
