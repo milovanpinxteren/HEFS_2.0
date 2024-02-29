85f1e1eea68bd6d64b63566f4f50d75eefded97d
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-02-29 17:13:12 +0100

    feat: start up scheduler after server is running

--- hefs/apps.py
+++ hefs/apps.py
@@ -12,9 +12,8 @@ class HighendfoodsolutionsConfig(AppConfig):
     def ready(self):
         print('APPS.PY ready function')
         print(sys.argv)
-        if 'runserver' in sys.argv:
-            print('runserver not')
-
+        if 'djangoProject.wsgi' in sys.argv:
+            print('server served')
             from djangoProject.scheduler import schedule_task
             schedule_task()
 
