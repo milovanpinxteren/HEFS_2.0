c3344676f3e1ad9414b31f87c5db218b7c376c5e
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-02-29 16:21:43 +0100

    feat: add task to scheduler

--- djangoProject/scheduler.py
+++ djangoProject/scheduler.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 from rq import Queue
 from rq.registry import ScheduledJobRegistry
-
+from django.conf import settings
 from redis import Redis
 from djangoProject.tasks import update_product_inventory
 
@@ -12,9 +12,11 @@ def schedule_task():
     redis_conn = Redis()
     # Create a queue
     queue = Queue(connection=redis_conn)
-    job = queue.enqueue_in(timedelta(seconds=10), update_product_inventory())
-    print(job in queue)  # Outputs False as job is not enqueued
-    print('job in queue, scheduler')
-    registry = ScheduledJobRegistry(queue=queue)
-    print(job in registry)
+    while True:
+        job = queue.enqueue(update_product_inventory)
+        print(job in queue)  # Outputs False as job is not enqueued
+        print('job in queue, scheduler')
+        registry = ScheduledJobRegistry(queue=queue)
+        print(job in registry)
+        time.sleep(settings.SCHEDULE_INTERVAL)
 
