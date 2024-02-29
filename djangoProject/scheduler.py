608188b5a078b10285d5462309dda88afc626321
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-02-29 17:21:15 +0100

    feat: remove double redis connection

--- djangoProject/scheduler.py
+++ djangoProject/scheduler.py
@@ -1,8 +1,10 @@
 
 import time
 from datetime import timedelta
+from os import getenv
 
 from django.dispatch import receiver
+from django_rq import get_queue
 from rq import Queue
 from rq.registry import ScheduledJobRegistry
 from django.conf import settings
@@ -11,9 +13,10 @@ from djangoProject.tasks import update_product_inventory
 
 
 def schedule_task():
-    redis_conn = Redis()
+    # redis_conn = Redis(host=getenv('REDIS_URL', 'localhost:6379'))
     # Create a queue
-    queue = Queue(connection=redis_conn)
+    # queue = Queue(connection=redis_conn)
+    queue = get_queue()
     while True:
         job = queue.enqueue(update_product_inventory)
         print(job in queue)  # Outputs False as job is not enqueued
