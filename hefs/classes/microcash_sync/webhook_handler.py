b37ddb896a81cbc26eee46901fff8ea8bc969376
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-03-19 18:15:46 +0100

    feat: improve logging on invoice sender

--- hefs/classes/microcash_sync/webhook_handler.py
+++ hefs/classes/microcash_sync/webhook_handler.py
@@ -18,36 +18,39 @@ class WebhookHandler():
         partner_websites = ['387f61-2.myshopify.com', 'gereiftebiere.de', '7c70bf.myshopify.com',
                             'gerijptebieren.myshopify.com', 'houseofbeers.nl', 'gerijptebieren.nl']  # both are the german, just to be sure
         request_domain = headers['x-shopify-shop-domain']
-        customer_name = json_body['customer']['first_name'] + ' ' + json_body['customer']['first_name']
+        customer_name = json_body['customer']['first_name'] + ' ' + json_body['customer']['last_name']
 
         if request_domain in partner_websites:
             if headers["x-shopify-topic"] == "orders/create":
                 self.error_handler.log_error('Order ontvangen ' + customer_name + request_domain)
-                xml_string = f"""
-                    <Order>
-                      <Reference>{json_body['name']}</Reference>
-                      <WebShop>House Of Beers</WebShop>
-                      <Naw>
-                        <Naam>{customer_name}</Naam>
-                        <Adres>{json_body['shipping_address']['address1']}</Adres>
-                        <Postcode>{json_body['shipping_address']['zip']}</Postcode>
-                        <Plaats>{json_body['shipping_address']['city']}</Plaats>
-                        <Email>{json_body['email']}</Email>
-                      </Naw>
-                      <Products>
-                """
-                for product in json_body['line_items']:
-                    xml_string += f"""
-                        <Product>
-                          <Barcode>{product['id']}</Barcode>
-                          <Amount>{product['fulfillable_quantity']}</Amount>
-                          <Price>{product['price']}</Price> 
-                        </Product>
-                                    """
-                xml_string += """
-                      </Products>
-                      <Note></Note>
-                    </Order>
-                        """
-                invoice_sender = InvoiceSender()
-                invoice_sender.send_invoice(xml_string)
+                try:
+                    xml_string = f"""
+                        <Order>
+                          <Reference>{json_body['name']}</Reference>
+                          <WebShop>House Of Beers</WebShop>
+                          <Naw>
+                            <Naam>{customer_name}</Naam>
+                            <Adres>{json_body['shipping_address']['address1']}</Adres>
+                            <Postcode>{json_body['shipping_address']['zip']}</Postcode>
+                            <Plaats>{json_body['shipping_address']['city']}</Plaats>
+                            <Email>{json_body['email']}</Email>
+                          </Naw>
+                          <Products>
+                    """
+                    for product in json_body['line_items']:
+                        xml_string += f"""
+                            <Product>
+                              <Barcode>{product['id']}</Barcode>
+                              <Amount>{product['fulfillable_quantity']}</Amount>
+                              <Price>{product['price']}</Price> 
+                            </Product>
+                                        """
+                    xml_string += """
+                          </Products>
+                          <Note></Note>
+                        </Order>
+                            """
+                    invoice_sender = InvoiceSender()
+                    invoice_sender.send_invoice(xml_string)
+                except Exception as e:
+                    self.error_handler.log_error('Order verzenden mislukt:, ' + e)
