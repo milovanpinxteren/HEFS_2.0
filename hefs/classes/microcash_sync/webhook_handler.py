fe05d9474bc3dca57cdefbc17a34d075a91beaa6
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-03-20 11:57:44 +0100

    feat: get correct product ID if order from different page

--- hefs/classes/microcash_sync/webhook_handler.py
+++ hefs/classes/microcash_sync/webhook_handler.py
@@ -4,11 +4,13 @@ from django.conf import settings
 
 from hefs.classes.error_handler import ErrorHandler
 from hefs.classes.microcash_sync.invoice_sender import InvoiceSender
+from hefs.classes.shopify.info_getter import InfoGetter
 
 
 class WebhookHandler():
     def __init__(self):
         self.error_handler = ErrorHandler()
+        self.info_getter = InfoGetter()
 
     def handle_request(self, headers, body):
         json_body = json.loads(body.decode('utf-8'))
@@ -23,6 +25,13 @@ class WebhookHandler():
         if request_domain in partner_websites:
             if headers["x-shopify-topic"] == "orders/create":
                 self.error_handler.log_error('Order ontvangen ' + customer_name + request_domain)
+                if request_domain != 'houseofbeers.nl':
+                    for product in json_body['line_items']:
+                        handle = self.info_getter.get_product_handle(product['id'])
+                        headers = {"Accept": "application/json", "Content-Type": "application/json",
+                                       "X-Shopify-Access-Token": settings.GERIJPTEBIEREN_ACCESS_TOKEN}
+                        productid, inventory_item_id = self.info_getter.get_ids_from_handle(handle, request_domain, headers)
+                        product['id'] = productid
                 try:
                     xml_string = f"""
                         <Order>
