13f67cd46dad7464aca3b36de9e071db4eac6f82
milovanpinxteren <milo.van.pinxteren@gmail.com>
2024-04-17 11:40:40 +0200

    feat: add logging to check full sync interval

--- djangoProject/tasks.py
+++ djangoProject/tasks.py
@@ -1,3 +1,4 @@
+from hefs.classes.error_handler import ErrorHandler
 from hefs.classes.microcash_sync.ftp_getter import FTPGetter
 from django_rq import get_queue
 from django.conf import settings
@@ -13,11 +14,14 @@ def update_product_inventory():
 
 def sync_all_products():
     ftp_getter = FTPGetter()
+    error_handler = ErrorHandler()
     ftp_getter.get_ftp_full_file()
     queue = get_queue()
     now = datetime.now()
     next_run = datetime(now.year, now.month, now.day, 23, 30)
     if now > next_run:
         next_run += timedelta(days=1)
-    print('enqueing full sync')
+    print('enqueing full sync for', next_run)
+    error_handler.log_error('enqueing full sync for' + str(next_run))
+
     queue.enqueue_at(next_run, sync_all_products)
