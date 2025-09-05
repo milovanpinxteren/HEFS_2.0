88d50200c42e91fced0e95d8c1c9f0f9db5ed945
Milo <milo.van.pinxteren@gmail.com>
2025-09-05 18:00:31 +0200

    feat: add data of 2024

--- hefs/classes/customer_info.py
+++ hefs/classes/customer_info.py
@@ -16,6 +16,7 @@ class CustomerInfo():
         data_2021 = JSONData.objects.get(key='data_2021').value
         data_2022 = JSONData.objects.get(key='data_2022').value
         data_2023 = JSONData.objects.get(key='data_2023').value
+        data_2024 = JSONData.objects.get(key='data_2024').value
         organisations_to_show = ApiUrls.objects.get(user_id=userid).organisatieIDs
         df_dates = pd.DataFrame.from_records(
             Orders.objects.filter(organisatieID__in=organisations_to_show).values_list('besteldatum'),
@@ -30,10 +31,10 @@ class CustomerInfo():
             df_dates.groupby(by=['Besteldatum'], as_index=False)['Bestellingen per dag'].sum())
         df_dates_grouped['Totaal aantal bestellingen'] = df_dates_grouped['Bestellingen per dag'].cumsum()
         df_dates_grouped['Totaal aantal personen'] = df_dates_grouped['Totaal aantal bestellingen'] * persons_per_order
-        df_dates_grouped['Jaar'] = 2024
+        df_dates_grouped['Jaar'] = 2025
 
         frames = [pd.DataFrame(data=data_2020), pd.DataFrame(data=data_2021), pd.DataFrame(data=data_2022), pd.DataFrame(data=data_2023),
-                  df_dates_grouped]
+                  pd.DataFrame(data=data_2024), df_dates_grouped]
         df_merged = pd.concat(frames).reset_index(drop=True)
 
         fig = px.line(df_merged, x="Besteldatum", y="Totaal aantal personen", color='Jaar',
@@ -52,22 +53,25 @@ class CustomerInfo():
         return div_orders_graph
 
     def important_numbers_table(self, userid):
-        organisations_to_show = ApiUrls.objects.get(user_id=userid).organisatieIDs
+        try:
+            organisations_to_show = ApiUrls.objects.get(user_id=userid).organisatieIDs
 
-        totale_inkomsten = float(Orders.objects.filter(organisatieID__in=organisations_to_show).aggregate(
-            Sum('orderprijs')).get('orderprijs__sum')) #gelijk aan inkomsten incl BTW
-        totale_verzendkosten = float(Orders.objects.filter(organisatieID__in=organisations_to_show).aggregate(
-            Sum('verzendkosten')).get('verzendkosten__sum'))
-        inkomsten_zonder_verzendkosten = totale_inkomsten - totale_verzendkosten
+            totale_inkomsten = float(Orders.objects.filter(organisatieID__in=organisations_to_show).aggregate(
+                Sum('orderprijs')).get('orderprijs__sum')) #gelijk aan inkomsten incl BTW
+            totale_verzendkosten = float(Orders.objects.filter(organisatieID__in=organisations_to_show).aggregate(
+                Sum('verzendkosten')).get('verzendkosten__sum'))
+            inkomsten_zonder_verzendkosten = totale_inkomsten - totale_verzendkosten
 
-        aantal_hoofdgerechten = AlgemeneInformatie.objects.get(naam='aantalHoofdgerechten').waarde
-        aantal_brunch = AlgemeneInformatie.objects.get(naam='aantalBrunch').waarde
-        aantal_orders = AlgemeneInformatie.objects.get(naam='aantalOrders').waarde
-        aantal_personen = aantal_hoofdgerechten + aantal_brunch
-        personen_per_order = aantal_personen / aantal_orders
-        gem_omzet_per_order = inkomsten_zonder_verzendkosten / aantal_orders
+            aantal_hoofdgerechten = AlgemeneInformatie.objects.get(naam='aantalHoofdgerechten').waarde
+            aantal_brunch = AlgemeneInformatie.objects.get(naam='aantalBrunch').waarde
+            aantal_orders = AlgemeneInformatie.objects.get(naam='aantalOrders').waarde
+            aantal_personen = aantal_hoofdgerechten + aantal_brunch
+            personen_per_order = aantal_personen / aantal_orders
+            gem_omzet_per_order = inkomsten_zonder_verzendkosten / aantal_orders
+            return aantal_hoofdgerechten, aantal_orders, personen_per_order, gem_omzet_per_order, aantal_personen
+        except Exception:
+            return 0,0,0,0,0
 
-        return aantal_hoofdgerechten, aantal_orders, personen_per_order, gem_omzet_per_order, aantal_personen
 
     def returning_customers_overview(self, userid):
         organisations_to_show = ApiUrls.objects.get(user_id=userid).organisatieIDs
@@ -75,6 +79,7 @@ class CustomerInfo():
         customers_2020 = Customers.objects.exclude(ordered_2020__isnull=True).count()
         customers_2021 = Customers.objects.exclude(ordered_2021__isnull=True).count()
         customers_2022 = Customers.objects.exclude(ordered_2022__isnull=True).count()
+        customers_2023 = 967
 
         returning_customers_2021 = Customers.objects.exclude(ordered_2020__isnull=True).exclude(
             ordered_2021__isnull=True).count()
@@ -92,7 +97,7 @@ class CustomerInfo():
 
         # Find the overlapping values
         returning_customers_2023 = len(set(values_model1) & set(values_model2))
-        return customers_2020, customers_2021, customers_2022, returning_customers_2021, returning_customers_2022, returning_customers_21_22, returning_customers_2023
+        return customers_2020, customers_2021, customers_2022, customers_2023, returning_customers_2021, returning_customers_2022, returning_customers_21_22, returning_customers_2023
 
     def orders_worth_table(self, userid):
         organisations_to_show = ApiUrls.objects.get(user_id=userid).organisatieIDs
@@ -100,13 +105,15 @@ class CustomerInfo():
         avg_orders_worth_2020 = 174.02307692307696
         avg_orders_worth_2021 = 172.8445392491467
         avg_orders_worth_2022 = 229.55841371918822
+        avg_orders_worth_2023 = 201.58
 
-        return avg_orders_worth_2020, avg_orders_worth_2021, avg_orders_worth_2022
+        return avg_orders_worth_2020, avg_orders_worth_2021, avg_orders_worth_2022, avg_orders_worth_2023
 
     def dinner_type_comparison(self, userid):
         organisations_to_show = ApiUrls.objects.get(user_id=userid).organisatieIDs
 
         distribution_2022 = JSONData.objects.get(key='distribution_2022').value
+        distribution_2023 = JSONData.objects.get(key='distribution_2023').value
         aantal_hoofdgerechten = AlgemeneInformatie.objects.get(naam='aantalHoofdgerechten').waarde
         aantal_brunches = AlgemeneInformatie.objects.get(naam='aantalBrunch').waarde
         aantal_personen = aantal_hoofdgerechten + aantal_brunches
@@ -115,10 +122,10 @@ class CustomerInfo():
         except ZeroDivisionError:
             percentage_brunch = 0
 
-        distribution_2023 = {"aantal personen": aantal_personen, "percentage brunch": percentage_brunch,
+        distribution_2024 = {"aantal personen": aantal_personen, "percentage brunch": percentage_brunch,
                              "aantal personen brunch": aantal_brunches
                              }
-        return distribution_2022, distribution_2023
+        return distribution_2022, distribution_2023, distribution_2024
 
     def prepare_view(self, userid):
         returning_customers_overview = self.returning_customers_overview(userid)
@@ -133,11 +140,13 @@ class CustomerInfo():
                    'aantal_personen': important_numbers[4],
                    'customers_2020': returning_customers_overview[0], 'customers_2021': returning_customers_overview[1],
                    'customers_2022': returning_customers_overview[2],
-                   'returning_customers_2021': returning_customers_overview[3],
-                   'returning_customers_2022': returning_customers_overview[4],
-                   'returning_customers_21_22': returning_customers_overview[5],
-                   'returning_customers_2023': returning_customers_overview[6],
+                   'customers_2023': returning_customers_overview[3],
+                   'returning_customers_2021': returning_customers_overview[4],
+                   'returning_customers_2022': returning_customers_overview[5],
+                   'returning_customers_21_22': returning_customers_overview[6],
+                   'returning_customers_2023': returning_customers_overview[7],
                    'avg_orders_worth_2020': orders_worth_table[0],
                    'avg_orders_worth_2021': orders_worth_table[1], 'avg_orders_worth_2022': orders_worth_table[2],
+                   'avg_orders_worth_2023': orders_worth_table[3],
                    'dinner_type_comparison': dinner_type_comparison}
         return context
